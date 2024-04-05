b73bcf2930e743980b6f808de7e8896114ffae1a
AstralSpire <p2656422@my365.dmu.ac.uk>
2024-04-05 16:25:33 +0100

    tried to add scene transition

---
+++ testingFYP/Assets/UI/Fade.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+   public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void FadeOut()
+    {
+        //animator.SetTrigger("FadeOut");
+    }
+
+}
